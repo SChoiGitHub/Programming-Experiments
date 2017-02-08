8c47e6fcf7a488ae3073d50990887f4e3f628221
Sherman Choi <22206740+SChoiGitHub@users.noreply.github.com>
2017-02-08 14:38:30 -0700

    Initial Push

---
+++ Haskell/circularStuff.hs
@@ -0,0 +1,4 @@
+--r squared pi is the area of a circle.
+circleArea r = r * r * pi
+--a * b * pi is the area of an elipse.
+ellipseArea a b = a * b * pi
---
+++ Haskell/experiment.hs
@@ -0,0 +1,4 @@
+--soup takes three variables, sums them then effectively doubles them.
+soup x y z = x + y + z + abba
+	where
+	abba = x + y + z
