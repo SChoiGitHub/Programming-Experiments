4dd8d2d3cd6f81e1baf2acbb203f144c115e267f
Sherman Choi <schoi@localhost.localdomain>
2017-06-05 18:47:14 -0500

    Changed animation; fixed memory leak like issue of not deleting object.

--- Javascript in HTML/Animation/script.js
+++ Javascript in HTML/Animation/script.js
@@ -4,28 +4,53 @@ var timer = 0;
 var width = parseInt(canvas.width);
 var height = parseInt(canvas.height);
 
-function block(){
-  this.x = Math.random()*width;
-  this.y = Math.random()*height;
-  this.speed = Math.random()*20-10;
+var entities = [];
+var num = 0;
+
+var mouse = {
+	x:0,
+	y:0
+};
+
+function line(x0,y0,x1,y1){
+	draw.beginPath();
+    draw.moveTo(x0, y0);
+    draw.lineTo(x1, y1);
+    draw.closePath();
+    draw.stroke();
 }
 
-var rendered = [];
-rendered.push(new block());
-setInterval(frame,10);
+canvas.addEventListener('mousemove', function(event){
+	mouse.x = event.clientX;
+	mouse.y = event.clientY;
+}, false);
 
+function something(h,s,location){
+	this.x = 0;
+	this.y = Math.random() * height;
+	this.hp = h;
+	this.spd = s;
+}
 
-function frame() {
-	rendered.push(new block());
-	draw.fillStyle = "#FFFFFF";
+setInterval(frame,20);
+
+function frame(){
+	entities.push(new something(Math.random()*100,Math.random()*25+1),num);
+	num += 1;
+	draw.fillStyle="#FFFFFF";
 	draw.fillRect(0,0,width,height);
 	
-	draw.fillStyle = "#FF0000";
-	rendered.forEach(function(b){
-		draw.fillRect(b.x,b.y,10,10);
-		b.y += b.speed;
-		if(b.y <= 0 || 600 <= b.y){
-			delete b;
+	draw.fillStyle="#00FF00";
+	
+	entities.forEach(function(a){
+		act(a);
+		draw.fillRect(a.x,a.y,15,15);
+		line(mouse.x,mouse.y,a.x,a.y);
+		if(a.x > width){
+			entities.splice(entities.indexOf(a),1);
 		}
 	});
+}
+function act(s){
+	s.x += s.spd;
 }
