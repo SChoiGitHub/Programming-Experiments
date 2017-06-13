ce86edab69c12312bdc4727e97a0dc61e44797f0
Sherman Choi <schoi@localhost.localdomain>
2017-06-12 19:05:54 -0500

    Attempted to create realistic collision between a falling, bouncy object and the wall. It did not work.

---
+++ Javascript in HTML/Bad Physics Experiment/script.js
@@ -0,0 +1,36 @@
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+var height = canvas.height;
+var width = canvas.width;
+
+
+
+setInterval(frame,25);
+
+class Ball{
+	constructor(InX,InY,InR){
+		this.X = InX;
+		this.y = InY;
+		this.r = InR;
+		this.vx = 0;
+		this.vy = 0;
+	}
+	fall(){
+		this.vx += Math.random()*4-2;
+		this.vy += Math.random()*4-2;
+		this.X += this.vx;
+		this.y += this.vy;
+		
+	}
+}
+var b = new Ball(600,588,5);
+
+function frame(){
+	b.fall();
+	context.fillStyle = "#FFFFFF";
+	context.fillRect(0,0,width,height);
+	context.beginPath();
+	context.arc(b.X,b.y,b.r,0,2*Math.PI);
+	context.stroke();
+}
+
--- Javascript in HTML/Scroll Around Test/script.js
+++ Javascript in HTML/Scroll Around Test/script.js
@@ -5,12 +5,32 @@ var width = canvas.width;
 
 var keys = {};
 var cameraCenter = {x:0,y:0};
+var mouse = {x:0,y:0};
+
+document.onmousemove = function(e){
+	mouse.x = e.clientX;
+	mouse.y = e.clientY;
+}
+
+canvas.onclick = function(){
+	for(key1 in f.g){
+		for(key2 in f.g[key1]){
+			if(f.g[key1][key2].clicked()){
+				f.g[key1][key2].activate();
+				break;
+			}
+		}
+	}
+	draw();
+}
 
 document.onkeydown = function(e){
 	keys[e.keyCode] = true;
+	draw();
 }
 document.onkeyup = function(e){
 	keys[e.keyCode] = false;
+	draw();
 }
 
 class field{
@@ -22,29 +42,43 @@ class field{
 		for(var y = 0; y < this.r; y++){
 			this.g[y] = {};
 			for(var x = 0; x < this.c; x++){
-				this.g[y][x] = new grid("b");
+				this.g[y][x] = new grid(this, "b",x,y);
 			}
 		}
 	}
 	draw(){
 		for(var y = 0; y < this.r; y++){
 			for(var x = 0; x < this.c; x++){
-				this.g[y][x].draw(x,y);
+				this.g[y][x].draw();
 			}
 		}
 	}
 }
 
 class grid{
-	constructor(type){
+	constructor(f, type, xIn, yIn){
+		this.f = f;
 		this.t = type;
-		this.who = null;
+		this.w = null;
+		this.s = 0;
+		this.x = xIn;
+		this.y = yIn;
+		this.a = false;
 	}
-	draw(xIn,yIn){
-		if(this.t == "b"){
+	draw(){
+		context.lineWidth = 3;
+		if(this.a){
+			context.strokeStyle = "#3333FF";
+		}else{
 			context.strokeStyle = "#000000";
-			context.strokeRect(cameraCenter.x+xIn*60+5,cameraCenter.y+yIn*60+5,50,50);
 		}
+		context.strokeRect(cameraCenter.x+this.x*60+5,cameraCenter.y+this.y*60+5,50,50);
+	}
+	clicked(){
+		return ((cameraCenter.x+this.x*60+5 < mouse.x && mouse.x < cameraCenter.x+this.x*60+55) && (cameraCenter.y+this.y*60+5 < mouse.y && mouse.y < cameraCenter.y+this.y*60+55));
+	}
+	activate(){
+		this.a = !this.a;
 	}
 }
 
@@ -62,14 +96,14 @@ function scroll(k){
 	}
 }
 
-
+	
 
 var f = new field("1",10,13);
+draw();
 
-
-setInterval(function(){ 
+function draw(){ 
 	context.fillStyle = "#FFFFFF";
 	context.fillRect(0,0,width,height);
 	scroll(keys);
 	f.draw();
-}, 30);
+}
