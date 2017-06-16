9c61f5ea37126fd106b36594621cc47b9f314e38
Sherman Choi <schoi@localhost.localdomain>
2017-06-15 19:14:08 -0500

    Bad physics experiment is not an okay simulation of a bouncing ball.

--- Javascript in HTML/Okay Physics Simulation/script.js
+++ Javascript in HTML/Okay Physics Simulation/script.js
@@ -14,23 +14,35 @@ class Ball{
 		this.vy = 0;
 	}
 	fall(){
-		this.vx += 0;
-		this.vy += 1;
+		this.vx = 3;
+		
 		this.X += this.vx;
 		this.y += this.vy;
-		if(this.y >= height){
-			this.vy = -1*this.vy;
-		}	
+		if(this.y+this.r >= height && this.vy >= 0){
+			this.vy = -0.95*this.vy;
+			this.y = height-this.r;
+		}else{
+			this.vy += 1;
+		}
+		context.fillStyle = "#000000";
+		context.fillText(this.X,100,100,100);
+		
+		if(this.X <= 0){
+			this.X = width - 1;
+		}else if(this.X >= width){
+			this.X = 1;
+		}
 	}
 }
 var b = new Ball(600,588,5);
 
 function frame(){
-	b.fall();
+	
 	context.fillStyle = "#FFFFFF";
 	context.fillRect(0,0,width,height);
 	context.beginPath();
 	context.arc(b.X,b.y,b.r,0,2*Math.PI);
 	context.stroke();
+	b.fall();
 }
 
